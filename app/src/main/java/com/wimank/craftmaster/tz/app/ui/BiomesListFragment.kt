3e3f6c7cea45242b2da890a2d8d9720c477b316c
Ivan Tananaev <ivan.tananaev@gmail.com>
2019-11-14 19:23:18 +0300

    feat: фрагменты для биомов

---
+++ app/src/main/java/com/wimank/craftmaster/tz/app/ui/BiomesListFragment.kt
@@ -0,0 +1,42 @@
+package com.wimank.craftmaster.tz.app.ui
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.wimank.craftmaster.tz.R
+
+class BiomesListFragment : Fragment() {
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_biomes_list, container, false)
+    }
+
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener)
+            listener = context
+        else
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+}
