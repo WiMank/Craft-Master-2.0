78e0c99bf99a093f4a2511eb5bc98292a684fb38
Ivan Tananaev <ivan.tananaev@gmail.com>
2019-11-24 18:48:49 +0300

    ref: обновлены стратегии MainActivityView

--- app/src/main/java/com/wimank/craftmaster/tz/app/mvp/views/MainActivityView.kt
+++ app/src/main/java/com/wimank/craftmaster/tz/app/mvp/views/MainActivityView.kt
@@ -16,16 +16,22 @@ interface MainActivityView : MvpView {
 
     fun showProgress(visibilityFlag: Boolean)
 
+    @StateStrategyType(SkipStrategy::class)
     fun showRecipesListFragment(section: String)
 
+    @StateStrategyType(SkipStrategy::class)
     fun showBlockAndItemsSection(item: String)
 
+    @StateStrategyType(SkipStrategy::class)
     fun showMobsSection(mob: String)
 
+    @StateStrategyType(SkipStrategy::class)
     fun showAchievementsSection()
 
+    @StateStrategyType(SkipStrategy::class)
     fun showBrewingSection()
 
+    @StateStrategyType(SkipStrategy::class)
     fun showBiomesSection(biome: String)
 
 }
