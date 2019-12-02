9d40a24de873539d1c98645ac45a2dd86dab4bc9
Ivan Tananaev <ivan.tananaev@gmail.com>
2019-12-02 16:41:01 +0300

    feat: SearchFragment

---
+++ app/src/main/java/com/wimank/craftmaster/tz/app/ui/SearchFragment.kt
@@ -0,0 +1,49 @@
+package com.wimank.craftmaster.tz.app.ui
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.wimank.craftmaster.tz.R
+
+class SearchFragment : Fragment() {
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        val a = listOf<String>("gg", "fthfgh", "fghfgh")
+
+
+
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener)
+            listener = context
+        else
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+}
